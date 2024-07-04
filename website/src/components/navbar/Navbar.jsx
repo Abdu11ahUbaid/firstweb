725b39d79dc55021261e8a9ad886d879e7fc4c2b
Abdu11ahUbaid <123170446+Abdu11ahUbaid@users.noreply.github.com>
2024-07-04 16:02:09 +0500

    navbar

---
+++ website/src/components/navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import logo from '../../assets/logo.png'
+import './Navbar.css'
+export default function Navbar() {
+  return (
+    <>
+    <div>Navbar 
+    <img src={logo} alt='image not found'/> 
+    <ul>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+
+    </ul>
+    </div>
+    </>
+  )
+}
+
